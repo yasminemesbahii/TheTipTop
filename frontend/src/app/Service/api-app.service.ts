b662af4130fe9a123178b1a693cdd4bb34d92503
yasminemesbahii <125924694+yasminemesbahii@users.noreply.github.com>
2023-02-26 13:09:04 +0100

    localhost to new IP

--- frontend/src/app/Service/api-app.service.ts
+++ frontend/src/app/Service/api-app.service.ts
@@ -8,30 +8,30 @@ import { LoginComponent } from '../login/login.component';
 export class ApiAppService {
   constructor(public http: HttpClient) {}
   login(userData: any) {
-    return this.http.post('http://localhost:7777/login', userData);
+    return this.http.post('http://34.16.134.192:7777/login', userData);
   }
   registerUser(userData: any) {
-    return this.http.post('http://localhost:7777/users', userData);
+    return this.http.post('http://34.16.134.192:7777/users', userData);
   }
   registerEMP(userData: any) {
-    return this.http.post('http://localhost:7777/users/EMP', userData);
+    return this.http.post('http://34.16.134.192:7777/users/EMP', userData);
   }
   toDashboard(token) {
     const headers = new HttpHeaders({
       'Content-type': 'application/json',
       authorization: `Bearer ${token}`,
     });
-    return this.http.get('http://localhost:7777/dashbord', { headers: headers });
+    return this.http.get('http://34.16.134.192:7777/dashbord', { headers: headers });
   }
   getPrixBy__v() {
-    return this.http.get('http://localhost:7777/users');
+    return this.http.get('http://34.16.134.192:7777/users');
   }
   sendMessage(nom: any, prenom: any, email: any, telephone: any, mesage: any) {
     const headers = new HttpHeaders({
       'Content-type': 'application/json',
     });
     return this.http.post(
-      'http://localhost:7777/email' +
+      'http://34.16.134.192:7777/email' +
         '/' +
         nom +
         '/' +
@@ -47,12 +47,12 @@ export class ApiAppService {
   }
 
   getReclamation() {
-    return this.http.get('http://localhost:7777/email');
+    return this.http.get('http://34.16.134.192:7777/email');
   }
   search() {
-    return this.http.get('http://localhost:7777/ChercherUserNop/');
+    return this.http.get('http://34.16.134.192:7777/ChercherUserNop/');
   }
   emailused(email: any) {
-    return this.http.get('http://localhost:7777/emailuse/' + email);
+    return this.http.get('http://34.16.134.192:7777/emailuse/' + email);
   }
 }
