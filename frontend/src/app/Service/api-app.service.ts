93a32d80b7f6379906fd05eee1833f95f44ecd07
yasminemesbahii <125924694+yasminemesbahii@users.noreply.github.com>
2023-03-06 19:53:00 +0100

    adding ssl (https)

--- frontend/src/app/Service/api-app.service.ts
+++ frontend/src/app/Service/api-app.service.ts
@@ -8,30 +8,30 @@ import { LoginComponent } from '../login/login.component';
 export class ApiAppService {
   constructor(public http: HttpClient) {}
   login(userData: any) {
-    return this.http.post('http://f2i-ao21b-ym-mel.fr/api/login', userData);
+    return this.http.post('https://f2i-ao21b-ym-mel.fr/api/login', userData);
   }
   registerUser(userData: any) {
-    return this.http.post('http://f2i-ao21b-ym-mel.fr/api/users', userData);
+    return this.http.post('https://f2i-ao21b-ym-mel.fr/api/users', userData);
   }
   registerEMP(userData: any) {
-    return this.http.post('http://f2i-ao21b-ym-mel.fr/api/users/EMP', userData);
+    return this.http.post('https://f2i-ao21b-ym-mel.fr/api/users/EMP', userData);
   }
   toDashboard(token) {
     const headers = new HttpHeaders({
       'Content-type': 'application/json',
       authorization: `Bearer ${token}`,
     });
-    return this.http.get('http://f2i-ao21b-ym-mel.fr/api/dashbord', { headers: headers });
+    return this.http.get('https://f2i-ao21b-ym-mel.fr/api/dashbord', { headers: headers });
   }
   getPrixBy__v() {
-    return this.http.get('http://f2i-ao21b-ym-mel.fr/api/users');
+    return this.http.get('https://f2i-ao21b-ym-mel.fr/api/users');
   }
   sendMessage(nom: any, prenom: any, email: any, telephone: any, mesage: any) {
     const headers = new HttpHeaders({
       'Content-type': 'application/json',
     });
     return this.http.post(
-      'http://f2i-ao21b-ym-mel.fr/api/email' +
+      'https://f2i-ao21b-ym-mel.fr/api/email' +
         '/' +
         nom +
         '/' +
@@ -47,12 +47,12 @@ export class ApiAppService {
   }
 
   getReclamation() {
-    return this.http.get('http://f2i-ao21b-ym-mel.fr/api/email');
+    return this.http.get('https://f2i-ao21b-ym-mel.fr/api/email');
   }
   search() {
-    return this.http.get('http://f2i-ao21b-ym-mel.fr/api/ChercherUserNop/');
+    return this.http.get('https://f2i-ao21b-ym-mel.fr/api/ChercherUserNop/');
   }
   emailused(email: any) {
-    return this.http.get('http://f2i-ao21b-ym-mel.fr/api/emailuse/' + email);
+    return this.http.get('https://f2i-ao21b-ym-mel.fr/api/emailuse/' + email);
   }
 }
